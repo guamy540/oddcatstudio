848918c32ef2b49e360055c6753207bbbace0150
Ryan High <ryan.high@pcc.edu>
2022-03-20 18:41:56 -0700

    fourth commit

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -7,7 +7,7 @@ import ReCAPTCHA from 'react-google-recaptcha'
 import { useState } from 'react'
 
 const ContactForm = () => {
-   const recaptchaKey = "6Lcl2PgeAAAAAJyMBH6IBNI96N2PH6T35jmiGVK1";
+   const recaptchaKey = "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI";
 
     function sendEmail(e){
         if (!isVerified){e.preventDefault();
